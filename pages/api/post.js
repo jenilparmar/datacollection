4f7da9f070dffea73f4f0929c379ce3f88413ff3
jenilparmar <jerryhash231@gmail.com>
2024-09-21 19:04:05 +0530

    Maths tests

--- pages/api/post.js
+++ pages/api/post.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         try {
             await client.connect();
             const database = client.db('dc');
-            const collection = database.collection('Chemistry_tests');
+            const collection = database.collection('Maths_tests');
             const data = req.body;
 
             const result = await collection.insertOne(data);
